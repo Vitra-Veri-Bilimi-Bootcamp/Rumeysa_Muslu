5defaaed053bf923a161a7572c373973ebfccbf4
rumeysamuslu <49349809+rmuslu@users.noreply.github.com>
2021-07-17 01:24:30 +0300

    Update Soru1.py

--- Hafta2-Python/Soru1.py
+++ Hafta2-Python/Soru1.py
@@ -1,11 +1,11 @@
 def flatten(input):
     new_list = []
-    def find_newlist(input):
+    def find_newlist(input): #to find all layers
         for item in input:
             if type(item) != list:
                 new_list.append(item)
             else:
-                find_newlist(item)
+                find_newlist(item) #if item type is list call find_newlist function for all layers
     find_newlist(input)
     print(new_list)
 
